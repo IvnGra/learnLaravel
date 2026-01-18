6b241cdfe3bac4882952e07dbbc689d540c45888
ivan gra <gratsov.ivan@gmail.com>
2026-01-18 17:38:18 +0200

    Add CRUD to tags

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -5,31 +5,35 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreTagRequest;
 use App\Http\Requests\UpdateTagRequest;
 use App\Models\Tag;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
 
 class TagController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): View
     {
-        //
+        $tags = Tag::paginate();
+        return view('tags.index', compact('tags'));
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): View
     {
-        //
+        return view('tags.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreTagRequest $request)
+    public function store(StoreTagRequest $request): RedirectResponse
     {
-        //
+        Tag::create($request->validated());
+        return redirect()->route('tags.index');
     }
 
     /**
@@ -43,24 +47,26 @@ class TagController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Tag $tag)
+    public function edit(Tag $tag): View
     {
-        //
+        return view('tags.edit', compact('tag'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateTagRequest $request, Tag $tag)
+    public function update(UpdateTagRequest $request, Tag $tag): RedirectResponse
     {
-        //
+        $tag->update($request->validated());
+        return redirect()->route('tags.index');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Tag $tag)
+    public function destroy(Tag $tag): RedirectResponse
     {
-        //
+        $tag->delete();
+        return redirect()->route('tags.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PublicController;
+use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PublicController::class, 'index'])->name('home');
@@ -32,6 +33,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/admin/posts/{post}/restore', [PostController::class, 'restore'])->name('posts.restore');
     Route::delete('/admin/posts/{post}/permadestroy', [PostController::class, 'permaDestroy'])->name('posts.permadestroy');
     Route::resource('/admin/posts', PostController::class);
+
+    Route::resource('/admin/tags', TagController::class);
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
